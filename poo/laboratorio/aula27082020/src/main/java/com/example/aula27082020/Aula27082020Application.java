a9e06f1b15b364f2f9c4effb676c1c0d5abc2f19
Gabriel Nogueira <gabrielnoog7@gmail.com>
2020-08-27 19:14:15 -0300

    criação estrutura projeto Maven com SpringBoot

---
+++ poo/laboratorio/aula27082020/src/main/java/com/example/aula27082020/Aula27082020Application.java
@@ -0,0 +1,13 @@
+package com.example.aula27082020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula27082020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula27082020Application.class, args);
+	}
+
+}
---
+++ poo/laboratorio/aula27082020/src/test/java/com/example/aula27082020/Aula27082020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aula27082020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula27082020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
