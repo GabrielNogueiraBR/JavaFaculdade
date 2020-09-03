cf466cb22d0de21c6df04caa9cc81d312a6cbced
Gabriel Nogueira <gabrielnoog7@gmail.com>
2020-09-02 22:21:49 -0300

    Utilização dos métodos e atributos de Carro

--- poo/teoria/aula03/TesteCarro.java
+++ poo/teoria/aula03/TesteCarro.java
@@ -3,19 +3,39 @@ package poo.teoria.aula03;
 public class TesteCarro {
     public static void main(String[] args) {
         Carro c1 = new Carro();
+        c1.codigo = 1;
 
-        c1.codigo = 100;
-        c1.velocidade = 100;
+        //Quando exibimos o c1, automaticamente é mostrado o to String, por isso o resultado da exibição é o mesmo
+        System.out.println(c1); 
+        System.out.println(c1.toString());
 
+        //exibindo o nome da classe no projeto e o nome simples da classe
+        System.out.println(c1.getClass().getName());
+        System.out.println(c1.getClass().getSimpleName());
 
-        System.out.println(c1); // vai ser exibido o hashcode da variável c1
-        System.out.println(c1.hashCode()); // valor do hashcode em hexadecimal
+        // valor do hashcode em decimal
+        System.out.println(c1.hashCode());
+
+        // valor do hashcode em hexadecimal
+        System.out.println( Integer.toHexString(c1.hashCode()));
+
         System.out.println(c1.codigo);
-        System.out.println(c1.velocidade);
-        System.out.println(c1.distanciaPercorrida); // como a distância não foi inicializada, o valor inicial é zero.
+        System.out.println(c1.velocidadeKmPorHora);
+        System.out.println(c1.distanciaPercorridaEmMetros); // como a distância não foi inicializada, o valor inicial é zero.
 
+        System.out.println("\n1º Movimento do carro: ");
+        c1.velocidadeKmPorHora = 100;
+        System.out.println("O carro moveu " + c1.mover(10) + " metros em 10 segundos" );
+        System.out.println(c1); 
         
+        System.out.println("\n2º Movimento do carro: ");
+        c1.velocidadeKmPorHora = 50;        
+        System.out.println("O carro moveu " + c1.mover(5) + " metros em 5 segundos" );
+        System.out.println(c1);
 
-
+        System.out.println("\n3º Movimento do carro: ");
+        c1.velocidadeKmPorHora = 80;
+        System.out.println("O carro moveu " + c1.mover(4) + " metros em 4 segundos" );
+        System.out.println(c1); 
     }
 }
