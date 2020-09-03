4ab93f2c25d98bdf059e40468eaa54e60d801c67
Gabriel Nogueira <gabrielnoog7@gmail.com>
2020-09-02 22:21:35 -0300

    Definição de métodos e atributos da Classe Carro

--- poo/teoria/aula03/Carro.java
+++ poo/teoria/aula03/Carro.java
@@ -2,8 +2,26 @@ package poo.teoria.aula03;
 
 public class Carro {
     int codigo;
-    double velocidade;
-    double distanciaPercorrida;
+    double velocidadeKmPorHora;
+    double distanciaPercorridaEmMetros;
+    
+
+    //Criação do metodo de movimento
+    public double mover(int segundos){
+        double dist = (this.velocidadeKmPorHora/3.6) * (segundos);
+        this.distanciaPercorridaEmMetros += dist;
+        return dist;
+    }
+
+
+
+    //Override é quando não queremos usar o toString original, ou seja estamos rescrevendo/sobrescrevendo o método toString
+    @Override
+    public String toString() {
+        return "Codigo do Carro=" + codigo + "\nDistância total=" + distanciaPercorridaEmMetros + "\nVelocidade atual=" + velocidadeKmPorHora;
+    }
+
+    
 
     
 }
