7cfbc19a8ae16e7edd4ad7b248969286e3d27f03
Gabriel Nogueira <gabrielnoog7@gmail.com>
2020-09-02 21:29:31 -0300

    Remoção de projetos java em SpringBoot
    Foi criado um repositório no GitHub para armazenar meus projetos Java usando o SpringBoot, por isso estou fazendo a exclusão dos itens neste repositório.

--- poo/teoria/aula03/TesteCarro.java
+++ poo/teoria/aula03/TesteCarro.java
@@ -2,7 +2,20 @@ package poo.teoria.aula03;
 
 public class TesteCarro {
     public static void main(String[] args) {
+        Carro c1 = new Carro();
+
+        c1.codigo = 100;
+        c1.velocidade = 100;
+
+
+        System.out.println(c1); // vai ser exibido o hashcode da variável c1
+        System.out.println(c1.hashCode()); // valor do hashcode em hexadecimal
+        System.out.println(c1.codigo);
+        System.out.println(c1.velocidade);
+        System.out.println(c1.distanciaPercorrida); // como a distância não foi inicializada, o valor inicial é zero.
+
         
+
 
     }
 }
