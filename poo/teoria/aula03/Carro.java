6e6adc8ab65511c8215c6bee4909dae2dc0be968
Gabriel Nogueira <gabrielnoog7@gmail.com>
2020-09-16 22:04:40 -0300

    Atualização da classe Carro

--- poo/teoria/aula03/Carro.java
+++ poo/teoria/aula03/Carro.java
@@ -5,18 +5,43 @@ public class Carro {
     private int codigo;
     private double velocidadeKmPorHora;
     private double distanciaPercorridaEmMetros;
+    private double tanque;
+    private double consumoEmKmPorLitros;
+
+    //funcao para abastecer o carro
+    public Boolean abastecer(double combustivel){
+        if(combustivel < 0)
+            return false;
+
+        if(combustivel > 50)
+            this.tanque = 50;
+
+        else
+            this.tanque = combustivel;
+
+        return true;
+
+    }
     
     //Criação do metodo de movimento
     public double mover(int segundos){
-        double dist = (this.velocidadeKmPorHora/3.6) * (segundos);
-        this.distanciaPercorridaEmMetros += dist;
+        
+        if(tanque > 0){
+            double dist = (this.velocidadeKmPorHora/3.6) * (segundos);
+            this.tanque -= (dist*0.001) * consumoEmKmPorLitros;
+            this.distanciaPercorridaEmMetros += dist;
         return dist;
+        }
+        else
+            return 0;
+        
+        
     }
 
     //Override é quando não queremos usar o toString original, ou seja estamos rescrevendo/sobrescrevendo o método toString
     @Override
     public String toString() {
-        return "\nCodigo do Carro=" + codigo + "\nDistância total=" + distanciaPercorridaEmMetros + "\nVelocidade atual=" + velocidadeKmPorHora;
+        return "\nCodigo do Carro=" + codigo + "\nDistância total(m)=" + distanciaPercorridaEmMetros + "\nVelocidade(Km/h)=" + velocidadeKmPorHora + "\nTanque(L): " + tanque + "\nConsumo (Km/L): " + consumoEmKmPorLitros;
     }
 
     //Getter de distancia percorrida em metros
@@ -44,5 +69,18 @@ public class Carro {
     //Setter de Velocidade Km Por Hora
     public void setVelocidadeKmPorHora(double velocidadeKmPorHora) {
         this.velocidadeKmPorHora = velocidadeKmPorHora;
+    }
+
+    public double getTanque() {
+        return tanque;
+    }
+
+   
+    public double getConsumo() {
+        return consumoEmKmPorLitros;
+    }
+
+    public void setConsumo(double consumoEmKmPorLitros) {
+        this.consumoEmKmPorLitros = consumoEmKmPorLitros;
     }
 }
